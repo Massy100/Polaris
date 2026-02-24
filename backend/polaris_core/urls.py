1d8325dfde26408075ff9aa6efe7ad19b0380c9c
Massy100 <massielle.coti@gmail.com>
2026-02-23 20:29:41 -0600

    feat(urls): set up main URL configuration and create app-specific url.py files

--- backend/polaris_core/urls.py
+++ backend/polaris_core/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/academic-career/', include('apps.academic_career.urls')),
+    path('api/academic-workload/', include('apps.academic_workload.urls')),
+    path('api/accounts/', include('apps.accounts.urls')),
+    path('api/assessment-360/', include('apps.assessment_360.urls')),
+    path('api/integrations/', include('apps.integrations.urls')),
+    path('api/notifications/', include('apps.notifications.urls')),
+    path('api/reporting/', include('apps.reporting.urls')),
+    path('api/security-audit/', include('apps.security_audit.urls')),
 ]
