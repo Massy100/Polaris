872845da6e46cae79850e36e25ab54c84b74c203
CarlsdeLeon <124699853+CarlsdeLeon@users.noreply.github.com>
2026-03-10 21:25:48 -0600

    fix(docker-compose): first step at fixing before testing
    (the polaris-backend not starting bug still exist)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  output: 'standalone', 
-  images: {
-    domains: ['localhost'], 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
+      },
+    ]
   },
-};
-
-export default nextConfig;
+}
