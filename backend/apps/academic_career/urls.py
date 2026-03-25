d83849fa513748f0d4b9f951ba34af35e2c50c8d
CarlsdeLeon <124699853+CarlsdeLeon@users.noreply.github.com>
2026-03-25 17:06:08 -0600

    fix(academic-career): the spanish comments are now errased

--- backend/apps/academic_career/urls.py
+++ backend/apps/academic_career/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TeacherViewSet
 
-# Crear el router
 router = DefaultRouter()
 router.register(r'teachers', TeacherViewSet, basename='teacher')
 
-# También puedes registrar otros ViewSets aquí si los tienes
 # router.register(r'careers', CareerViewSet, basename='career')
 # router.register(r'courses', CourseViewSet, basename='course')
 # router.register(r'faculties', FacultyViewSet, basename='faculty')
