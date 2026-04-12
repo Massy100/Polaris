1d497c5b383bc43c0dab2878867df574134a5470
CarlsdeLeon <124699853+CarlsdeLeon@users.noreply.github.com>
2026-04-12 15:03:04 -0600

    fix(backend): fix the backend

--- backend/apps/academic_career/urls.py
+++ backend/apps/academic_career/urls.py
@@ -1,14 +1,13 @@
 # academic-career/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeacherViewSet
+from .views import TeacherViewSet, CareerViewSet, CourseViewSet, FacultyViewSet
 
 router = DefaultRouter()
 router.register(r'teachers', TeacherViewSet, basename='teacher')
-
-# router.register(r'careers', CareerViewSet, basename='career')
-# router.register(r'courses', CourseViewSet, basename='course')
-# router.register(r'faculties', FacultyViewSet, basename='faculty')
+router.register(r'careers', CareerViewSet, basename='career')
+router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'faculties', FacultyViewSet, basename='faculty')
 
 urlpatterns = [
     path('', include(router.urls)),
